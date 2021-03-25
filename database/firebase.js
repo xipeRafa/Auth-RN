189f39468694ff78cb89d8403c5c443fb8b92e6e
rafa <rafaelsistem87@gmail.com>
2021-03-25 08:46:51 -0700

    audit fix

---
+++ database/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+   apiKey: "AIzaSyDn4PlWC_U1PaDs8iRs_Qnm3y4P0c9SuxE",
+   authDomain: "login-5c355.firebaseapp.com",
+   projectId: "login-5c355",
+   storageBucket: "login-5c355.appspot.com",
+   messagingSenderId: "731841673314",
+   appId: "1:731841673314:web:ffb431570a6a542189b93b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
